f6820d4a624e4c320be9c5a1b4498eda7ea0d54b
Alexey Andreev <sailen.alex@gmail.com>
2017-08-09 16:27:48 +0300

    NotifyingLimitSettings to restrict multiple repeated indicator notifications

---
+++ MonitoringService/MonitoringService/Startup.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+using Core.Settings;
+using MonitoringService.Dependencies;
+using AzureRepositories;
+using Core.Services;
+
+namespace MonitoringService
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            var settings = GetSettings(Configuration);
+            services.AddSingleton<IBaseSettings>(settings.MonitoringService);
+            services.AddSingleton<INotifyingLimitSettings, NotifyingLimitSettings>();
+            services.AddSingleton<ISlackNotificationSettings>(settings.SlackNotifications);
+            services.RegisterAzureLogs(settings.MonitoringService);
+            services.RegisterAzureStorages(settings.MonitoringService);
+            services.RegDependencies();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MonitoringService", Version = "v1" });
+            });
+            // Add framework services.
+            services.AddMvc();
+
+            ServiceProvider = services.BuildServiceProvider();
+            return ServiceProvider;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
+        {
+            #region Application LifeTime
+
+            applicationLifetime.ApplicationStopping.Register(() => 
+            {
+                var backupService = ServiceProvider.GetService<IBackUpService>();
+
+                backupService.CreateBackupAsync().Wait();
+            });
+
+            applicationLifetime.ApplicationStarted.Register(() =>
+            {
+                var backupService = ServiceProvider.GetService<IBackUpService>();
+
+                backupService.RestoreBackupAsync().Wait();
+            });
+
+            #endregion
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseCors((policyBuilder) =>
+            {
+                policyBuilder.AllowAnyHeader();
+                policyBuilder.AllowAnyOrigin();
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MonitoringService");
+            });
+        }
+
+        static SettingsWrapper GetSettings(IConfigurationRoot configuration)
+        {
+            var connectionString = configuration.GetConnectionString("ConnectionString");
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new Exception("ConnectionString is empty");
+
+            var settings = GeneralSettingsReader.ReadGeneralSettings<SettingsWrapper>(connectionString);
+
+            return settings;
+        }
+    }
+}
---
+++ MonitoringService/Services/MonitoringJob.cs
@@ -0,0 +1,178 @@
+﻿using Common.Log;
+using Core.Exceptions;
+using Core.Jobs;
+using Core.Models;
+using Core.Repositories;
+using Core.Services;
+using Core.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class MonitoringJob : IMonitoringJob
+    {
+        private readonly IMonitoringService _monitoringService;
+        private readonly IBaseSettings _settings;
+        private readonly ISlackNotifier _slackNotifier;
+        private IApiHealthCheckErrorRepository _apiHealthCheckErrorRepository;
+        private readonly IIsAliveService _isAliveService;
+        private readonly INotifyingLimitSettings _notifyingLimitSettings;
+
+        public MonitoringJob(IMonitoringService monitoringService,
+            IBaseSettings settings,
+            ISlackNotifier slackNotifier,
+            IApiMonitoringObjectRepository apiMonitoringObjectRepository,
+            IApiHealthCheckErrorRepository apiHealthCheckErrorRepository,
+            IIsAliveService isAliveService,
+            INotifyingLimitSettings notifyingLimitSettings,
+            ILog log)
+        {
+            _log = log;
+            _monitoringService = monitoringService;
+            _settings = settings;
+            _slackNotifier = slackNotifier;
+            _isAliveService = isAliveService;
+            _notifyingLimitSettings = notifyingLimitSettings;
+            _apiHealthCheckErrorRepository = apiHealthCheckErrorRepository;
+        }
+
+        public async Task CheckJobs()
+        {
+            DateTime now = DateTime.UtcNow;
+            IEnumerable<IMonitoringObject> jobsMonitoring = (await GetMonitoringObjectsForProcessing(x => string.IsNullOrEmpty(x.Url)))?.ToList();
+            List<IMonitoringObject> fireNotificationsFor = new List<IMonitoringObject>();
+
+            foreach (var @object in jobsMonitoring)
+            {
+                if (now - @object.LastTime > TimeSpan.FromSeconds(_settings.MaxTimeDifferenceInSeconds))
+                {
+                    fireNotificationsFor.Add(@object);
+                }
+            }
+
+            fireNotificationsFor.ForEach(async service =>
+            {
+                var timeDiff = now - service.LastTime;
+                string formattedDiff = timeDiff.ToString(@"hh\:mm\:ss");
+                await _slackNotifier.ErrorAsync($"No updates from {service.ServiceName} within {formattedDiff}!");
+            });
+        }
+
+        public async Task CheckAPIs()
+        {
+            IEnumerable<IMonitoringObject> apisMonitoring = (await GetMonitoringObjectsForProcessing(x => !string.IsNullOrEmpty(x.Url)))?.ToList();
+            List<Task<IApiStatusObject>> pendingHttpChecks = new List<Task<IApiStatusObject>>(apisMonitoring.Count());
+            List<ApiHealthCheckError> failedChecks = new List<ApiHealthCheckError>(apisMonitoring.Count());
+            IDictionary<string, IMonitoringObject> serviceNameMonitoringObjectMapping = apisMonitoring.ToDictionary(x => x.ServiceName);
+            IDictionary<Task<IApiStatusObject>, IMonitoringObject> requestServiceMapping = new Dictionary<Task<IApiStatusObject>, IMonitoringObject>();
+            DateTime now = DateTime.UtcNow;
+
+            foreach (var api in apisMonitoring)
+            {
+                CancellationTokenSource cts = new CancellationTokenSource(5500);
+
+                Task<IApiStatusObject> task = _isAliveService.GetStatusAsync(api.Url, cts.Token);
+                pendingHttpChecks.Add(task);
+                requestServiceMapping[task] = api;
+            }
+
+            List<Task> recipientChecks = new List<Task>(apisMonitoring.Count());
+            foreach (var item in pendingHttpChecks)
+            {
+                string serviceName = requestServiceMapping[item].ServiceName;
+
+                var task = Task.Run(async () =>
+                {
+                    try
+                    {
+                        IApiStatusObject statusObject = await item;
+                        requestServiceMapping[item].Version = statusObject.Version;
+                        requestServiceMapping[item].LastTime = now;
+
+                        //If api send any failing indicators, they are added to error output
+                        GenerateError(failedChecks, now, statusObject.IssueIndicators, serviceName);
+                    }
+                    catch (OperationCanceledException e)
+                    {
+                        GenerateError(failedChecks, now, "Timeout", serviceName);
+                    }
+                    catch (SimpleHttpResponseException e)
+                    {
+                        GenerateError(failedChecks, now, e.StatusCode.ToString(), serviceName);
+                    }
+                    catch (Exception e)
+                    {
+                        await _log.WriteErrorAsync("MonitoringJob", "CheckAPIs", "", e, DateTime.UtcNow);
+                        GenerateError(failedChecks, now, $"Unexpected exception: {e.Message}", serviceName);
+                    }
+                });
+
+                recipientChecks.Add(task);
+            }
+
+            await Task.WhenAll(recipientChecks);
+
+            foreach (var error in failedChecks)
+            {
+                IMonitoringObject mObject = serviceNameMonitoringObjectMapping[error.ServiceName];
+                await _apiHealthCheckErrorRepository.InsertAsync((IApiHealthCheckError)error);
+                await _slackNotifier.ErrorAsync($"Service url check failed for {error.ServiceName}(URL:{mObject.Url}), reason: {error.LastError}!");
+            }
+
+            foreach (var api in apisMonitoring)
+            {
+                await _monitoringService.Ping(api);
+            }
+        }
+
+        #region Private
+
+        private object failedChecksLock = new object();
+        private ILog _log;
+
+        private void GenerateError(List<ApiHealthCheckError> errors, DateTime now, 
+            IEnumerable<IssueIndicatorObject> issueIndicators, string serviceName)
+        {
+            var indicators = _notifyingLimitSettings.CheckAndUpdateLimits(serviceName, issueIndicators);
+
+            if (indicators.Count == 0)
+                return;
+
+            var message = string.Join("; ", indicators.Select(o => o.Type + ": " + o.Value));
+            GenerateError(errors, now, message, serviceName);
+        }
+
+        private void GenerateError(List<ApiHealthCheckError> errors, DateTime now, string errorMessage, string serviceName)
+        {
+            var error = new ApiHealthCheckError()
+            {
+                Date = now,
+                LastError = errorMessage,
+                ServiceName = serviceName,
+            };
+
+            lock (failedChecksLock)
+            {
+                errors.Add(error);
+            }
+        }
+
+        private async Task<IEnumerable<IMonitoringObject>> GetMonitoringObjectsForProcessing(Func<IMonitoringObject, bool> filter)
+        {
+            var now = DateTime.UtcNow;
+            Func<IMonitoringObject, bool> decoratedFilter = (@object) => !(@object.SkipCheckUntil > now) && filter(@object);
+            var allMonitoringObjects = await _monitoringService.GetCurrentSnapshot();
+            var filteredObjects = allMonitoringObjects.Where(@object => decoratedFilter(@object));
+
+            return filteredObjects;
+        }
+
+        #endregion
+    }
+}
